4954964cc37e0a890567f835c3ed1d9cccf0b52c
Perkles <hkelvin798@gmail.com>
2024-11-03 20:46:26 -0300

    adicionei javascript

---
+++ assets/scripts.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleCheckbox = document.getElementById('toggle');
+    const homeLink = document.getElementById('home');
+    const aboutLink = document.getElementById('about');
+    const projectsLink = document.getElementById('projects');
+
+    homeLink.addEventListener('click', () => {
+        toggleCheckbox.checked = false; 
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+    aboutLink.addEventListener('click', (event) => {
+        event.preventDefault(); 
+        toggleCheckbox.checked = false; 
+        window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' }); 
+    });
+
+    projectsLink.addEventListener('click', () => {
+        toggleCheckbox.checked = false; 
+    });
+});
